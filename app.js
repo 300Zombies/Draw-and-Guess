62917ce14278f3656f5703e07c7b014954c441aa
300Spartans <TNDark@gmail.com>
2019-08-27 17:43:47 +0800

    modules

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// TODO: server
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//     res.send("hello world");
+// });
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
