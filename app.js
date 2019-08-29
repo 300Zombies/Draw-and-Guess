6d1a68e3a22e36cebb885c49eaedda0b6822e741
300Spartans <TNDark@gmail.com>
2019-08-29 18:01:11 +0800

    socket.IO chat

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const server = app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
+const io = require("socket.io").listen(server);
 
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//     res.send("hello world");
-// });
+app.use(bodyParser.urlencoded({ // urlencoded is <form>'s default data type
+    extended: false
+}));
+app.use(bodyParser.json()); // now body-parser can parse <form>'s json
 
-app.listen(3000, () => {
-    console.log("listening on port 3000");
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("chat message", function (msg) {
+        io.emit("chat message", msg);
+    });
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
 });
--- public/static/canvas.js
+++ public/static/canvas.js
@@ -1,11 +1,14 @@
 window.addEventListener("load", () => {
+    const frame = document.querySelector("#canvas-frame");
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
-
+    const rect = canvas.getBoundingClientRect();
     // resizing inside window onload
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+    // canvas.width = 500;
+    // canvas.height = 500;
+    console.log(canvas.offsetWidth, canvas.offsetHeight)
     // variables
     let drawing = false;
 
@@ -21,16 +24,21 @@ window.addEventListener("load", () => {
         ctx.beginPath();
     }
 
+
     function draw(e) {
         if (!drawing) return;
         console.log("drawing");
-        ctx.lineWidth = 10;
+        ctx.lineWidth = 2;
         ctx.lineCap = "round";
-        ctx.lineTo(e.clientX, e.clientY);
+
+        ctx.lineTo(e.clientX - rect.left, e.clientY - rect.top);
         ctx.stroke();
         ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
+        ctx.moveTo(e.clientX - rect.left, e.clientY - rect.top);
+        console.log(e.clientX, e.clientY);
+        console.log(rect.left, rect.top)
     }
+    // console.log(canvas.offsetLeft, canvas.offsetTop);
     // eventListeners
     canvas.addEventListener("mousedown", start);
     canvas.addEventListener("mouseup", finish);
